e7dcae414832291cf23c2e7b3e38cf22947de5f9
GabrielDC57 <gabriel.dupire.caissial@gmail.com>
2024-09-08 20:51:32 +0200

    Changer le test pour avoir des variables en minuscule

--- test.py
+++ test.py
@@ -3,18 +3,30 @@
 # =================================
 # Date : 2024-09-04
 
-print("Tu te reveilles")
 
-nom_du_heros=input("Comment tu t'appelles ? ")
+print("=======================================")
+print("DEBUT DU JEU")
+print("=======================================")
 
-Jesor=input(nom_du_heros + ", tu sors? Oui ou Non ?")
-if Jesor=="Oui":
-    print("D'accord " + nom_du_heros + ". Je vois. Tu préfères sortir.")
+print("- Tu te reveilles")
+
+nom_du_heros=input("- Comment tu t'appelles ?:")
+
+Jesor=input("- " + nom_du_heros + ", tu sors ? Oui ou Non ?:")
+if Jesor.lower()=="oui":
+    print("- D'accord " + nom_du_heros + ". Je vois. Tu préfères sortir.")
 else:
-    print("Bahhhh " + nom_du_heros + "! TU FAIS QUOI LA ? Tu te casses ?") 
+    print("- Bahhhh " + nom_du_heros + "! TU FAIS QUOI LA ? Tu te casses ?") 
 
-kir=input("Tu vas dans le Jardin ? oui ou non") 
-if kir=="oui"
-    print("Ok.Tu as obtenu une épèe solide.") 
+kir=input("- Tu vas dans le Jardin ? oui ou non ?:") 
+
+if kir.lower()=="oui":
+    print("- Ok.Tu as obtenu une épèe solide.") 
 else:
-    print("")
+    print("- OK.")
+
+
+
+print("=======================================")
+print("FIN DU JEU")
+print("=======================================")
