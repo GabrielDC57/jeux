8a7cdc90f4fdcc3ae7896b6ef8a8afc3511454bd
GabrielDC57 <gabriel.dupire.caissial@gmail.com>
2024-09-08 20:42:46 +0200

    First commit

---
+++ boucle.py
@@ -0,0 +1,3 @@
+for n in range(10):
+    print("POUAAAHHHHHH --> " + str(n))
+
---
+++ test.py
@@ -0,0 +1,20 @@
+# =================================
+# Jeux d'aventure de Gabriel Dupire
+# =================================
+# Date : 2024-09-04
+
+print("Tu te reveilles")
+
+nom_du_heros=input("Comment tu t'appelles ? ")
+
+Jesor=input(nom_du_heros + ", tu sors? Oui ou Non ?")
+if Jesor=="Oui":
+    print("D'accord " + nom_du_heros + ". Je vois. Tu préfères sortir.")
+else:
+    print("Bahhhh " + nom_du_heros + "! TU FAIS QUOI LA ? Tu te casses ?") 
+
+kir=input("Tu vas dans le Jardin ? oui ou non") 
+if kir=="oui"
+    print("Ok.Tu as obtenu une épèe solide.") 
+else:
+    print("")
