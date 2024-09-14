e30fa517978bafc388bf582adedb9c67d7a53afe
GabrielDC57 <gabriel.dupire.caissial@gmail.com>
2024-09-14 12:33:44 +0200

    Ajout de commentaire

---
+++ guides3.py
@@ -0,0 +1,10 @@
+def prout():
+    """Methode ou fonction qui réalise les trois étapes en dessous"""
+    print("Prout")
+    print("Camion ca fait prout")
+    print()
+
+prout()
+prout()
+prout()
+prout()
